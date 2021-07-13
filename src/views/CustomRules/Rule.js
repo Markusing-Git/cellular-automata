dc8333885ba898fe4fe33498cc278fb6f7947489
ingem <markuscontacting@gmail.com>
2021-07-13 17:28:42 +0200

    Customizable rule blocks added

---
+++ src/views/CustomRules/Rule.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Rule.css';
+
+const Rule = () => (
+  <div className="Rule">
+    <div className="Block" />
+    <div className="Block" />
+    <div className="Block" />
+    <div className="Block" />
+  </div>
+);
+
+export default Rule;
--- src/views/CustomRules/index.js
+++ src/views/CustomRules/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './CustomRules.css';
+import Rule from './Rule';
 
 const CustomRules = () => (
   <div className="CustomRulesContainer">
     <div className="CustomRulesPanel">
       <div className="Heading">Customize your own rules</div>
+      <div className="RulesWrapper">
+        <Rule />
+        <Rule />
+        <Rule />
+        <Rule />
+        <Rule />
+        <Rule />
+        <Rule />
+        <Rule />
+      </div>
     </div>
   </div>
 );
