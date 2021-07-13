6f1e68f37f21c47daae54ea1cf234647f1dcae46
ingem <markuscontacting@gmail.com>
2021-07-13 15:08:57 +0200

    custom rules component added

--- src/views/App/index.js
+++ src/views/App/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
 import GridView from '../GridView';
+import CustomRules from '../CustomRules';
 
 const App = () => (
-  <div className="App"><GridView /></div>
+  <div className="App">
+    <GridView />
+    <CustomRules />
+  </div>
 );
 
 export default App;
