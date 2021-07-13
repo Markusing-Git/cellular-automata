31233770afa0ad29d1d4443601c4d0f86662561d
ingem <markuscontacting@gmail.com>
2021-07-13 15:08:57 +0200

    custom rules component added

---
+++ src/views/CustomRules/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CustomRules.css';
+
+const CustomRules = () => (
+  <div className="CustomRulesContainer">
+    <div className="CustomRulesPanel">
+      <div className="Heading">Customize your own rules</div>
+    </div>
+  </div>
+);
+
+export default CustomRules;
