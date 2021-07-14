f7186fcb73a8ba9d9f08ff7d3861d06be7982da4
ingem <markuscontacting@gmail.com>
2021-07-14 14:36:08 +0200

    interactivity and state handling added for customRules

--- src/views/CustomRules/index.js
+++ src/views/CustomRules/index.js
@@ -2,19 +2,14 @@ import React from 'react';
 import './CustomRules.css';
 import Rule from './Rule';
 
-const CustomRules = () => (
+const CustomRules = ({ rules, setRules }) => (
   <div className="CustomRulesContainer">
     <div className="CustomRulesPanel">
       <div className="Heading">Customize your own rules</div>
       <div className="RulesWrapper">
-        <Rule />
-        <Rule />
-        <Rule />
-        <Rule />
-        <Rule />
-        <Rule />
-        <Rule />
-        <Rule />
+        {Object.keys(rules).map((key) => (
+          <Rule pattern={key} setRules={setRules} />
+        ))}
       </div>
     </div>
   </div>
