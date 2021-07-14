5c65e983faf62a03d82d1688bfdada46bf2381c2
ingem <markuscontacting@gmail.com>
2021-07-14 19:38:50 +0200

    transition added

--- src/views/GridView/Grid/Cell.js
+++ src/views/GridView/Grid/Cell.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './Grid.css';
 
-const Cell = ({ row, col, isAlive }) => (
-  <div
-    id={`Cell-${row}-${col}`}
-    className="Cell"
-    style={isAlive ? { backgroundColor: 'black' } : { backgroundColor: 'white' }}
-  />
-);
+const Cell = ({ row, col, isAlive }) => {
+  const extraClassName = isAlive ? 'Cell-alive' : '';
+
+  const animation = {
+    transition: 'background-color 0.5s ease',
+    transitionDelay: `${(row / 50)}s`,
+  };
+
+  return (
+    <div
+      id={`Cell-${row}-${col}`}
+      className={`Cell ${extraClassName}`}
+      style={animation}
+    />
+  );
+};
 
 export default Cell;
